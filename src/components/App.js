9d501c3c1f38363bcfa16f92feba9e459246749e
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-23 16:28:14 +0530

    code-added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import SignInSide from "./Login.jsx";
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <SignInSide />
+    </div>
+  );
+};
 
 export default App;
