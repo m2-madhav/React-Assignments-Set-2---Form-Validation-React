46591acf63413a159de5f1268ddb1f87c70a983f
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-10 09:36:43 +0530

    code-added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,55 +1,136 @@
 import React from "react";
 
-export default function SignUp() {
-  const [userValue, setuserValue] = React.useState("");
-  const handleClick = () => {
-    let msr = document.getElementById("email").value;
-    let str = msr.match(/^([^@]*)@/)[1];
-    setuserValue(str);
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      gender: "male",
+      phoneNumber: "",
+      password: "",
+      errorMessage: "",
+      userName: ""
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
   };
-  return (
-    <div>
-      <label for="name">Name: </label>
-      <input type="text" data-testid="name" />
-      <br />
-      <br />
-      <label for="email">Email: </label>
-      <input type="email" id="email" data-testid="email" />
-      <br />
-      <br />
-      <label for="gender" data-testid="gender">
-        Gender:{" "}
-      </label>
-      <br />
-      <input type="radio" name="gender" value="male" />
-      <label for="male">Male</label>
-      <br />
-      <input type="radio" name="gender" value="female" />
-      <label for="female">Female</label>
-      <br />
-      <input type="radio" name="gender" value="other" />
-      <label for="other">Other</label>
-      <br />
-      <br />
-      <label for="number">Phone Number: </label>
-      <input type="number" data-testid="phoneNumber" />
-      <br />
-      <br />
-      <label for="password">Password: </label>
-      <input type="password" data-testid="password" />
-      <br />
-      <br />
-      <label for="submit">Submit:</label>
-      <button
-        type="submit"
-        id="submit"
-        data-testid="submit"
-        onClick={handleClick}
-      >
-        Submit
-      </button>
-      <br />
-      {userValue}
-    </div>
-  );
+
+  handleSubmit = () => {
+    const alphaNumeric = /^[0-9a-zA-Z]+$/;
+    const numbers = /^\d+$/;
+    if (
+      this.state.name === "" ||
+      this.state.email === "" ||
+      this.state.phoneNumber === "" ||
+      this.state.gender === "" ||
+      this.state.password === ""
+    ) {
+      this.setState({ errorMessage: "All fields are mandatory", userName: "" });
+      return;
+    }
+    if (!this.state.name.match(alphaNumeric)) {
+      this.setState({ errorMessage: "Name is not alphanumeric", userName: "" });
+      return;
+    }
+    if (this.state.email.indexOf("@") < 1) {
+      this.setState({ errorMessage: "Email must contain @", userName: "" });
+      return;
+    }
+
+    if (!this.state.gender) {
+      this.setState({
+        errorMessage: "Please identify as male, female or others",
+        userName: ""
+      });
+      return;
+    }
+    if (!numbers.test(this.state.phoneNumber)) {
+      this.setState({
+        errorMessage: "Phone Number must contain only numbers",
+        userName: ""
+      });
+      return;
+    }
+    if (this.state.password.length < 6) {
+      this.setState({
+        errorMessage: "Password must contain atleast 6 letters",
+        userName: ""
+      });
+      return;
+    }
+    const user = this.state.email.substring(0, this.state.email.indexOf("@"));
+    this.setState({
+      userName: user,
+      errorMessage: "",
+      name: "",
+      email: "",
+      gender: "male",
+      phoneNumber: "",
+      password: ""
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        Name:
+        <input
+          data-testid="name"
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        Email:
+        <input
+          data-testid="email"
+          type="text"
+          name="email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        Gender:
+        <input
+          data-testid="gender"
+          type="text"
+          name="gender"
+          value={this.state.gender}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        PhoneNumber:
+        <input
+          data-testid="phoneNumber"
+          type="text"
+          name="phoneNumber"
+          value={this.state.phNo}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        Password:
+        <input
+          data-testid="password"
+          type="password"
+          name="password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <br />
+        <br />
+        <button data-testid="submit" onClick={this.handleSubmit}>
+          Submit
+        </button>
+        {this.state.errorMessage && <div>{this.state.errorMessage}</div>}
+        {this.state.userName && <div>Hello {this.state.userName}</div>}
+      </div>
+    );
+  }
 }
